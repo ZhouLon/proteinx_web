d730dddeb9e550afe7c569a09bc4762374fd737f
ZhouLon <1922450589@qq.com>
2026-01-19 22:18:30 +0800

    static_sphinx

--- develop/document/01_dl/source/conf.py
+++ develop/document/01_dl/source/conf.py
@@ -14,7 +14,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,6 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
+html_permalinks_icon = '<span>#</span>'
