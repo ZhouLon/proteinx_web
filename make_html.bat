d839da0ad4ccc83b77c10363d64c22dac808f468
ZhouLon <1922450589@qq.com>
2026-01-31 17:45:24 +0800

    update 1.10&1.11

--- make_html.bat
+++ make_html.bat
@@ -4,10 +4,10 @@
 if exist build rmdir /s /q build
 
 :: 创建必要的目录结构
-mkdir build\nginx_static\document >nul 2>&1
+mkdir build\nginx-static\document >nul 2>&1
 
 :: 复制文档
-xcopy "develop\document\01_dl\build\html\*" "build\nginx_static\document\01_dl\" /E /I /Y
+xcopy "develop\document\01_dl\build\html\*" "build\nginx-static\document\01_dl\" /E /I /Y
 
 :: 复制主要页面
 xcopy "develop\main_pages\*" "build\main_pages\" /E /I /Y
